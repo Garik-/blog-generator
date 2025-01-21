0bec60e6fde4ad68ea70e1151f254ee907f204fc
Gar|k <garik.djan@gmail.com>
2025-01-21 23:30:52 +0300

    fix: download script

--- bin/download_site.sh
+++ bin/download_site.sh
@@ -13,6 +13,13 @@ function exit_on_error {
 rm -rf $DIST_DIR
 
 echo "Download site $TARGET_URL to $DIST_DIR"
-wget -r -P $DIST_DIR -nH $TARGET_URL &> /dev/null || exit_on_error "download error"
-cp -r $PUBLIC_DIR/. $DIST_DIR
-find $DIST_DIR -name "*.css.map" | xargs rm
+wget -nc -r -P $DIST_DIR -nH $TARGET_URL 2>/dev/null
+export RC=$?
+if [ "$RC" = "0" ]; then
+   echo $1 OK
+   cp -r $PUBLIC_DIR/. $DIST_DIR
+   find $DIST_DIR -name "*.css.map" | xargs rm
+else
+    echo $RC FAILED
+    exit 1
+fi
